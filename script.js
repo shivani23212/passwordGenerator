953106af9b4f444eba54fd7ddb1a73f522c39744
shivani <shivani23212@gmail.com>
2022-04-19 17:26:47 +0100

    Removed unnecessary comments

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const uppercase = lowercase.map(function(x) {return x.toUpperCase();});
 const numbersArr = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialChars = ['!', '£', '$', '%', '&', '*', '(', ')'];
 
-// need to make copy button function and add footer, then upload to stackoverflow
-// don't forget to message about zerogravity
-
 
 output.innerHTML = slider.value; // update value with slider input val
 
